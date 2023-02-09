e69e3d5bb3bd2ad50a6d78ce55bf48db5048e7e8
Hammed Oyedele <itz.harmid@gmail.com>
2023-02-09 15:17:18 +0100

    feat: improve the socket server

--- start/socket.ts
+++ start/socket.ts
@@ -9,6 +9,8 @@ Ws.boot();
  * Listen for incoming socket connections.
  */
 Ws.on('connection', (socket) => {
+  console.log('new socket', socket.id);
+
   socket.emit('news', { hello: 'world' });
 
   socket.on('my other event', (data) => {
