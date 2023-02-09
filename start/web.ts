ff2af975dc96cdfca55a6c6c32083fd1838eb497
Hammed Oyedele <itz.harmid@gmail.com>
2023-02-09 16:17:58 +0100

    refactor: improve route registrations

--- start/web.ts
+++ start/web.ts
@@ -12,7 +12,7 @@ import Route from '@ioc:Adonis/Core/Route';
 function registerRoutes() {
   Route.get('/', async ({ view }) => {
     return view.render('welcome');
-  }).as('home');
+  }).as('welcome');
 
   Route.get('/socket', async ({ view }) => {
     return view.render('socket');
