ff55f5f848097be2e09ca568ffcd9a5b7d8f119a
Hammed Oyedele <itz.harmid@gmail.com>
2023-02-09 16:06:22 +0100

    test: add assertions for socket page

--- tests/functional/hello-world.spec.ts
+++ tests/functional/hello-world.spec.ts
@@ -5,5 +5,15 @@ test('display welcome page', async ({ client }) => {
 
   response.assertStatus(200);
 
-  response.assertTextIncludes('<h1 class="title"> It Works! </h1>');
+  response.assertTextIncludes('<h1 class="title">It Works!</h1>');
+});
+
+test('display socket page', async ({ client }) => {
+  const response = await client.get('/socket');
+
+  response.assertStatus(200);
+
+  response.assertTextIncludes(
+    '<p class="subtitle">Check the console for socket messages.</p>'
+  );
 });
