09e9c53f84859641373187d94a3392f4939a70d2
Hammed Oyedele <itz.harmid@gmail.com>
2023-02-10 12:26:53 +0100

    refactor: move the socket server start file back to app provider

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -31,7 +31,9 @@ export default class AppProvider {
   }
 
   public async ready() {
-    // App is ready
+    if (this.app.environment === 'web') {
+      await import('../start/socket');
+    }
   }
 
   public async shutdown() {
