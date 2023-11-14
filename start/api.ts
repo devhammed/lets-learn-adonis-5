df81278e869b1d72ba584f5bdaff3ed4202605f1
Hammed Oyedele <itz.harmid@gmail.com>
2023-11-14 16:45:13 +0100

    refactor: improve general api definition

--- start/api.ts
+++ start/api.ts
@@ -11,7 +11,8 @@ import Route from '@ioc:Adonis/Core/Route';
 
 Route
     .group(() => {
-      Route.resource('users', 'UsersController').only(['store', 'show']);
+        Route.resource('users', 'UsersController')
+            .only(['store', 'show']);
     })
     .as('api.v1')
     .prefix('api/v1')
--- start/web.ts
+++ start/web.ts
@@ -9,18 +9,18 @@
 
 import Route from '@ioc:Adonis/Core/Route';
 
-function registerRoutes() {
-  Route.get('/', async ({ view }) => {
-    return view.render('welcome');
-  }).as('welcome');
+Route
+    .group(() => {
+        Route.get('/', async ({ view }) => {
+          return view.render('welcome');
+        }).as('welcome');
 
-  Route.get('/socket', async ({ view }) => {
-    return view.render('socket');
-  }).as('socket');
+        Route.get('/socket', async ({ view }) => {
+          return view.render('socket');
+        }).as('socket');
 
-  Route.get('/react', async ({ view }) => {
-    return view.render('react');
-  }).as('react');
-}
-
-export default Route.group(registerRoutes).as('web');
+        Route.get('/react', async ({ view }) => {
+          return view.render('react');
+        }).as('react');
+    })
+    .as('web');
