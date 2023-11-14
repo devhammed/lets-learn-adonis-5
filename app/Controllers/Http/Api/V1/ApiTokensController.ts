7d032d639e5831863e3a45e7fe3149ec34c13cf2
Hammed Oyedele <itz.harmid@gmail.com>
2023-11-14 22:51:50 +0100

    style: reformat code

--- app/Controllers/Http/Api/V1/ApiTokensController.ts
+++ app/Controllers/Http/Api/V1/ApiTokensController.ts
@@ -1,45 +1,45 @@
 import ApiToken from 'App/Models/ApiToken';
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import ApiTokensValidator from 'App/Validators/Api/V1/ApiTokensValidator';
 
 export default class ApiTokensController {
     /**
      * Create a new user token.
      */
-    public async store({ request, auth, bouncer }: HttpContextContract) {
-       return bouncer
-           .with('ApiTokenPolicy')
-           .authorize('create')
-           .then(async () => {
-               const { email, password } = await request.validate(ApiTokensValidator);
+    public async store({request, auth, bouncer}: HttpContextContract) {
+        return bouncer
+            .with('ApiTokenPolicy')
+            .authorize('create')
+            .then(async () => {
+                const {email, password} = await request.validate(ApiTokensValidator);
 
-               try {
-                   const {
-                       token,
-                       type,
-                       expiresAt,
-                   } = await auth
-                       .use('api')
-                       .attempt(email, password, { expiresIn: '30 days' });
+                try {
+                    const {
+                        token,
+                        type,
+                        expiresAt,
+                    } = await auth
+                        .use('api')
+                        .attempt(email, password, {expiresIn: '30 days'});
 
-                   return {
-                       ok: true,
-                       data:{
-                           token,
-                           type,
-                           expiresAt: expiresAt?.toUnixInteger(),
-                       },
-                   };
-               } catch {
-                   return { ok: false, message: 'Invalid credentials.' };
-               }
-           });
+                    return {
+                        ok: true,
+                        data: {
+                            token,
+                            type,
+                            expiresAt: expiresAt?.toUnixInteger(),
+                        },
+                    };
+                } catch {
+                    return {ok: false, message: 'Invalid credentials.'};
+                }
+            });
     }
 
     /**
      * Revoke user token.
      */
-    public async destroy({ params, bouncer, response }: HttpContextContract) {
+    public async destroy({params, bouncer, response}: HttpContextContract) {
         const apiToken = await ApiToken.findOrFail(params.id);
 
         await bouncer.with('ApiTokenPolicy').authorize('delete', apiToken);
