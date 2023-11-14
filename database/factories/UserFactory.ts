d2a0eb1fd55a38b43b683f31567414143b057bbc
Hammed Oyedele <itz.harmid@gmail.com>
2023-11-14 19:12:29 +0100

    feat: add user factory

--- database/factories/UserFactory.ts
+++ database/factories/UserFactory.ts
@@ -2,14 +2,14 @@ import User from 'App/Models/User'
 import Factory from '@ioc:Adonis/Lucid/Factory'
 
 export default Factory
-    .define(User, ({ faker }) => {
+    .define(User, ({faker}) => {
         const firstName = faker.name.firstName();
         const lastName = faker.name.lastName();
 
         return {
-          name: [firstName, lastName].join(' '),
-          email: faker.internet.email(firstName, lastName),
-          password: 'password',
+            name: [firstName, lastName].join(' '),
+            email: faker.internet.email(firstName, lastName),
+            password: 'password',
         };
     })
     .build();
