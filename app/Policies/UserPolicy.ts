7bd06eeb35aa742656ccf7e02f956d2943854dc5
Hammed Oyedele <itz.harmid@gmail.com>
2023-11-14 22:23:09 +0100

    feat: add user policies viewAny and delete methods

--- app/Policies/UserPolicy.ts
+++ app/Policies/UserPolicy.ts
@@ -2,6 +2,10 @@ import User from 'App/Models/User';
 import {BasePolicy, action} from '@ioc:Adonis/Addons/Bouncer';
 
 export default class UserPolicy extends BasePolicy {
+    public async viewAny(_user: User) {
+        return false;
+    }
+
     public async view(user: User, model: User) {
         return user.id === model.id;
     }
@@ -13,5 +17,9 @@ export default class UserPolicy extends BasePolicy {
 
     public async update(user: User, model: User) {
         return user.id === model.id;
+    }
+
+    public async delete(_user: User, _model: User) {
+        return false;
     }
 }
