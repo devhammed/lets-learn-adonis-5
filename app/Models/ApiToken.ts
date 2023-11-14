81dbad706ef2de63b742a083e1a6ce6b003cafae
Hammed Oyedele <itz.harmid@gmail.com>
2023-11-14 22:58:40 +0100

    feat: complete api token model

--- app/Models/ApiToken.ts
+++ app/Models/ApiToken.ts
@@ -9,12 +9,12 @@ export default class ApiToken extends BaseModel {
   @column()
   public userId: number;
 
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>;
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime;
-
-  @belongsTo(() => User)
-  public user: BelongsTo<typeof User>;
 }
